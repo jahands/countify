7f9360fa15368e25cde682eb50679f6b18ff8f7f
Jacob Hands <jacob@gogit.io>
2023-06-07 06:23:29 -0500

    Add comments

--- apps/api/src/routes.ts
+++ apps/api/src/routes.ts
@@ -3,8 +3,11 @@ export const routes = {
     counter: {
       /** Matches all actions for a namespace/name */
       all: '/:action/:namespace/:name',
+      /** Creates a new counter in a namespace */
       new: '/new/:namespace/:name',
+      /** Gets the value of a counter */
       get: '/get/:namespace/:name',
+      /** Increments a counter */
       inc: '/inc/:namespace/:name',
     }
   }
