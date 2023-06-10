38ba54d7ef419ea8a0575653d75657cbd7bdf074
Jacob Hands <jacob@gogit.io>
2023-06-10 09:24:27 -0500

    Update eslint config

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,11 +1,24 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
-  rules: {
-    "@next/next/no-html-link-for-pages": "off",
-  },
-  parserOptions: {
-    babelOptions: {
-      presets: [require.resolve("next/babel")],
-    },
-  },
-};
+	extends: ['next', 'turbo', 'prettier', 'eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+	rules: {
+		'@next/next/no-html-link-for-pages': 'off',
+		// disable the rule for all files
+		'@typescript-eslint/explicit-function-return-type': 'off',
+	},
+	parser: '@typescript-eslint/parser',
+	plugins: ['@typescript-eslint'],
+	overrides: [
+		{
+			// enable the rule specifically for TypeScript files
+			files: ['*.ts', '*.mts', '*.cts', '*.tsx'],
+			rules: {
+				'@typescript-eslint/explicit-function-return-type': 'error',
+			},
+		},
+	],
+	parserOptions: {
+		babelOptions: {
+			presets: [require.resolve('next/babel')],
+		},
+	},
+}
