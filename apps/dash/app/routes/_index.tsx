b51c6c30cfa797b27f5f3ed80f81801819338b27
Jacob Hands <jacob@gogit.io>
2023-06-15 14:14:55 -0500

    formatting

--- apps/dash/app/routes/_index.tsx
+++ apps/dash/app/routes/_index.tsx
@@ -1,16 +1,9 @@
-import type { V2_MetaFunction } from "@remix-run/cloudflare";
+import type { V2_MetaFunction } from '@remix-run/cloudflare'
 
 export const meta: V2_MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
+	return [{ title: 'New Remix App' }, { name: 'description', content: 'Welcome to Remix!' }]
+}
 
 export default function Index() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  );
+	return <h1 className="text-3xl font-bold underline">Hello world!</h1>
 }
